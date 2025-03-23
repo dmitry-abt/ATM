f2ad85b61cf9a9dbadea87122f099dff6db7024c
Dmitry Abt <dmitry.abt@gmail.com>
2025-03-23 18:50:52 +0700

    added the simplest ATM interface

---
+++ ATM/ATMViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel;
+
+namespace Bank
+{
+    public class ATMViewModel : INotifyPropertyChanged
+    {
+        private ATM atm;
+
+        public ATMViewModel()
+        {
+            atm = new ATM();
+        }
+
+        public int Balance => atm.GetBalance();
+
+        public List<Banknote> Banknotes => atm.Banknotes;
+
+        public void Deposit(int denomination, int count)
+        {
+            atm.Deposit(denomination, count);
+            OnPropertyChanged(nameof(Balance));
+            OnPropertyChanged(nameof(Banknotes));
+        }
+
+        public bool Withdraw(int amount)
+        {
+            bool result = atm.Withdraw(amount);
+            OnPropertyChanged(nameof(Balance));
+            OnPropertyChanged(nameof(Banknotes));
+            return result;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- ATM/MainWindow.xaml.cs
+++ ATM/MainWindow.xaml.cs
@@ -2,14 +2,31 @@
 
 namespace Bank
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private ATMViewModel viewModel;
+
         public MainWindow()
         {
             InitializeComponent();
+            viewModel = new ATMViewModel();
+            DataContext = viewModel;
+        }
+
+        private void Withdraw_Click(object sender, RoutedEventArgs e)
+        {
+            // -500 EUR
+            bool success = viewModel.Withdraw(500);
+            if (!success)
+            {
+                MessageBox.Show("Insufficient funds or the specified amount cannot be issued");
+            }
+        }
+
+        private void Deposit_Click(object sender, RoutedEventArgs e)
+        {
+            // +100 EUR 
+            viewModel.Deposit(100, 1);
         }
     }
 }
